b84f9d911d4b433d4e806601f46dc98e3253cc8e
TiyaReba <tiyarebaken@gmail.com>
2022-06-26 01:18:27 +0530

    modifications done

--- js/login.js
+++ js/login.js
@@ -1,33 +1,81 @@
 let email = document.getElementById("email");
 let pwd = document.getElementById("pwd");
 let error = document.getElementById("error");
-function validate(){
-    var regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/
-    var pasWord = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
-
-    if(regexp.test(email.value)){
-        error.innerHTML = "Valid";
-        error.style.color ="green";
-        // return true;
-    }
-    else{
-        error.innerHTML = "Invalid";
-        error.style.color = "red";
-        email.style.border = "2px solid red";
-        // return false;
-    }
-    if(pasWord.test(pwd.value)){
-        error3.innerHTML = "Valid";
-        error3.style.color ="green";
-        return true;
-
-    }
-    else{
-            error3.innerHTML = "Invalid";
-            error3.style.color = "red";
-            pwd.style.border = "2px solid red";
-            return false
+let error3 = document.getElementById("error3");
+let form = document.getElementsByClassName("login")[0];
+let Password = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})')
 
 
-}
-}
+
+
+    function validate(){
+        var regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/;
+       
+        var pasWord =/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+        // var pasWord = /^[A-Z]{1,8}[a-z]{1,8}[0-9]{1,8}$/
+    
+    
+        if(email.value.trim() ==""){
+            error.innerHTML = "Email cannot be empty";
+            error.style.color = "red";
+            email.style.border = "2px solid red";
+            return false;
+    
+        }
+    
+        else if(!regexp.test(email.value)){
+            error.innerHTML = "Invalid";
+            error.style.color = "red";
+            email.style.border = "2px solid red";
+            return false;
+    
+        }
+        else {
+            error.innerHTML = "";
+            error.style.color ="green";
+            email.style.border = "2px solid green";
+            return true;  
+        }
+        
+        }
+
+        function passwordChecker(){
+            if(pwd.value==""){
+                error3.innerHTML = "Password cannot be empty";
+                    error3.style.color = "red";
+                    pwd.style.border = "2px solid red";
+                    return false;
+            
+            }
+            else if(!Password.test(pwd.value)){
+                error3.innerHTML = "Invalid";
+                error3.style.color = "red";
+                pwd.style.border = "2px solid red";
+                return false;
+            }
+
+            else {
+                error3.innerHTML = "";
+                error3.style.color ="green";
+                pwd.style.border = "2px solid green";
+                return true;
+        
+            }
+        }
+        email.addEventListener("input", () => {
+            validate();
+        })
+        pwd.addEventListener("input", () => {
+            passwordChecker();
+        })
+        form.addEventListener('submit', function (e) {
+
+            if(!validate() ||  !passwordChecker()) {
+            // prevent the form from submitting
+            e.preventDefault();
+        }
+        
+        });        
+
+
+
--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,5 @@
 let email = document.getElementById("email");
-// let pwd = document.getElementById("pwd");
+let form = document.getElementsByClassName("login")[0];
 let pwd2 = document.getElementById("pwd2");
 let phnum = document.getElementById("phnum");
 let error = document.getElementById("error");
@@ -14,71 +14,84 @@ let strengthBadge = document.getElementById('StrengthDisp');
 let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})')
 let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.{8,}))')
 
+// checking username
 function validate(){
     var regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/;
-    var phNum = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+   
     var pasWord =/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
     // var pasWord = /^[A-Z]{1,8}[a-z]{1,8}[0-9]{1,8}$/
 
-    
-    
-    if(regexp.test(email.value)){
-        error.innerHTML = "Valid";
-        error.style.color ="green";
-        email.style.border = "2px solid green";
-        // return true;
 
-      
+    if(email.value.trim() ==""){
+        error.innerHTML = "Email cannot be empty";
+        error.style.color = "red";
+        email.style.border = "2px solid red";
+        return false;
+
     }
-    else if{
+
+    else if(!regexp.test(email.value)){
         error.innerHTML = "Invalid";
         error.style.color = "red";
         email.style.border = "2px solid red";
-        // return false;
+        return false;
 
     }
-    else if(phNum.test(phnum.value)){
-        error2.innerHTML = "Valid";
-        error2.style.color ="green";
-        // return true;
-
+    else {
+        error.innerHTML = "";
+        error.style.color ="green";
+        email.style.border = "2px solid green";
+        return true;  
     }
-    else if{
+    
+    }
+
+// checking phone no
+function validatePhNumb (){
+    var phNum = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+    if(phnum.value.trim()==""){
+            error2.innerHTML = "Phone No cannot be empty";
+            error2.style.color = "red";
+            phnum.style.border = "2px solid red";
+            return false;
+    }
+   
+    else if(!phNum.test(phnum.value)){
             error2.innerHTML = "Invalid";
             error2.style.color = "red";
             phnum.style.border = "2px solid red";
-            // return false
+            return false;
     }
-    // if(pasWord.test(pwd.value)){
-    //     error3.innerHTML = "Valid";
-    //     error3.style.color ="green";
-    //     // return true;
+    else {
+        error2.innerHTML = "";
+        error2.style.color ="green";
+        phnum.style.border = "2px solid green";
+        return true;
 
-    // }
-    // else{
-    //         error3.innerHTML = "Invalid";
-    //         error3.style.color = "red";
-    //         pwd.style.border = "2px solid red";
-    //         // return false
-    // } 
-    // if(pasWord.test(pwd2.value)){
-    //     error4.innerHTML = "Valid";
-    //     error4.style.color ="green";
-    //     return true;
-
-    // }
-    // else{
-    //         error4.innerHTML = "Invalid";
-    //         error4.style.color = "red";
-    //         pwd2.style.border = "2px solid red";
-    //         return false
-    // } 
+    }
+    
 }
-  
+function passwordChecker(){
+    if(password.value==""){
+        error3.innerHTML = "Password cannot be empty";
+            error3.style.color = "red";
+            password.style.border = "2px solid red";
+            return false;
+    
+    }
+    else {
+        error3.innerHTML = "";
+        error3.style.color ="green";
+        password.style.border = "2px solid green";
+        return true;
+
+    }
+}
 
 function StrengthChecker(PasswordParameter){
     // We then change the badge's color and text based on the password strength
-
+   
     if(strongPassword.test(PasswordParameter)) {
         strengthBadge.style.backgroundColor = "green"
         strengthBadge.textContent = 'Strong'
@@ -89,8 +102,20 @@ function StrengthChecker(PasswordParameter){
         strengthBadge.style.backgroundColor = 'red'
         strengthBadge.textContent = 'Weak'
     }
+
 }
 
+email.addEventListener("input", () => {
+    validate();
+})
+
+phnum.addEventListener("input", () => {
+    validatePhNumb();
+})
+
+pwd2.addEventListener("input", () => {
+   check();
+})
 // Adding an input event listener when a user types to the  password input 
 
 password.addEventListener("input", () => {
@@ -98,11 +123,7 @@ password.addEventListener("input", () => {
     //The badge is hidden by default, so we show it
 
     strengthBadge.style.display= 'block'
-    clearTimeout(timeout);
-
-    //We then call the StrengChecker function as a callback then pass the typed password to it
-
-    timeout = setTimeout(() => StrengthChecker(password.value), 500);
+    StrengthChecker(password.value);
 
     //Incase a user clears the text, the badge is hidden again
 
@@ -112,14 +133,33 @@ password.addEventListener("input", () => {
         strengthBadge.style.display = 'none'
     }
 });
+   
+    form.addEventListener('submit', function (e) {
 
-// confirm password checking
+        if(!validate() || !validatePhNumb() || !passwordChecker() ||!check()){
+        // prevent the form from submitting
+        e.preventDefault();
+    }
+    
+    });
+    
+    
 
-// function check() {
-//     if(password.value ===
-//             pwd2.value) {
-//         document.getElementById('message').innerHTML = "match";
-//     } else {
-//         document.getElementById('message').innerHTML = "no match";
-//     }
-// }
+function check() {
+    if(password.value ===
+            pwd2.value) {
+            error4.innerHTML = "";
+            error4.style.color = "green";
+            pwd2.style.border = "2px solid green";
+            return true;
+    
+    }
+    else {
+        error4.innerHTML = "Password does not match";
+        error4.style.color ="red";
+        password.style.border="2px solid red";
+        pwd2.style.border = "2px solid red";
+        return false;
+
+    }
+}
